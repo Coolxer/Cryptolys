6214709bd46cbf01b72fae628026b73b458e1ca3
coolxer <lukasik4789@gmail.com>
2018-09-23 14:59:34 +0200

    added footer component with css file

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../css/Footer.css';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="row">
+                
+            </div>
+        );
+    }
+}
+
+export default Footer;
