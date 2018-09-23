13357f387709ac3723197f71431822c2247745d6
coolxer <lukasik4789@gmail.com>
2018-09-23 14:58:21 +0200

    added content component with css file

---
+++ src/components/Content.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../css/Content.css';
+
+class Content extends Component {
+    render() {
+        return (
+            <div className="row">
+            
+            </div>
+        );
+    }
+}
+
+export default Content;
