a72310fc082bc518671db21bf4421b04ff9a63d3
coolxer <lukasik4789@gmail.com>
2018-09-23 14:50:54 +0200

    customized projects structure and tested if it works

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+     <h2>elo</h2>
+    );
+  }
+}
+
+export default App;
