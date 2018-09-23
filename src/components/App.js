cc5088bf05fb875e2ced9bee6b2846e125f68aeb
coolxer <lukasik4789@gmail.com>
2018-09-23 14:56:36 +0200

    made that boostrap is working fine

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import '../css/App.css';
 import Menu from './Menu';
 
 class App extends Component {
   render() {
     return (
-      <Menu />
+      <div className="container-fluid">
+        <Menu />
+      </div>
     );
   }
 }
